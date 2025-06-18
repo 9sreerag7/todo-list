cda81bc13c974463978904007bf4e859ab31b573
Sundara Siva Sreerag <151757051+9sreerag7@users.noreply.github.com>
2025-06-18 18:57:27 +0530

    Create TodoList.jsx

---
+++ TodoList.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Todo from "./Todo";
+
+export default function TodoList({ todos, toggleComplete, deleteTodo }) {
+  return (
+    <ul className="todo-list">
+      {todos.map((todo) => (
+        <Todo
+          key={todo.id}
+          todo={todo}
+          toggleComplete={toggleComplete}
+          deleteTodo={deleteTodo}
+        />
+      ))}
+    </ul>
+  );
+}
